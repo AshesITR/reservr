9103157eab5ec696609b8da47760dd11e8f46926
Alexander Rosenstock <alexander.rosenstock@web.de>
2022-06-05 21:38:23 +0200

    fix pgamma_diff_matrix for scalar lower, upper

--- src/dgamma_matrix.cpp
+++ src/dgamma_matrix.cpp
@@ -15,14 +15,17 @@ NumericMatrix dgamma_matrix(NumericVector x, NumericVector shape, double scale) 
 
 // [[Rcpp::export]]
 NumericMatrix pgamma_diff_matrix(const NumericVector lower, const NumericVector upper, const NumericVector shape, const NumericVector scale) {
-  R_xlen_t n = std::max(lower.size(), scale.size()), m = shape.size();
+  int n = std::max(lower.size(), scale.size()), m = shape.size();
   NumericMatrix res(n, m);
-  R_xlen_t i_scale = 0, d_scale = scale.size() > 1 ? 1 : 0;
-  for (R_xlen_t i = 0; i < n; i++) {
-    for (R_xlen_t j = 0; j < m; j++) {
-      res(i, j) = R::pgamma(upper[i], shape[j], scale[i_scale], 1, 0) - R::pgamma(lower[i], shape[j], scale[i_scale], 1, 0);
+  int i_scale = 0, d_scale = scale.size() > 1 ? 1 : 0;
+  int i_interval = 0, d_interval = lower.size() > 1 ? 1 : 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      res(i, j) = R::pgamma(upper[i_interval], shape[j], scale[i_scale], 1, 0) -
+        R::pgamma(lower[i_interval], shape[j], scale[i_scale], 1, 0);
     }
     i_scale += d_scale;
+    i_interval += d_interval;
   }
   return res;
 }
