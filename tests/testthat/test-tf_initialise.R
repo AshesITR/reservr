32dfd4b0c9f7abb3eca80c9480f3a99f7160965a
Alexander Rosenstock <alexander.rosenstock@web.de>
2023-05-13 14:32:30 +0200

    capture random weight from TF directly

--- tests/testthat/test-tf_initialise.R
+++ tests/testthat/test-tf_initialise.R
@@ -1,7 +1,6 @@
 test_that("test tf_initialize", {
   skip_if_no_tensorflow()
   set.seed(1337L)
-  tensorflow::set_random_seed(1337L)
   tensorflow::tf$keras$backend$set_floatx("float32")
   on.exit({ tensorflow::tf$keras$backend$set_floatx("float64") })
 
@@ -23,13 +22,21 @@ test_that("test tf_initialize", {
   )
 
   bias <- log(exp(global_fit$params$rate) - 1)
+
+  tensorflow::set_random_seed(1337L)
+  random_weight <- as.numeric(
+    tensorflow::tf$random$uniform(list(1L), minval = -0.1, maxval = 0.1, dtype = keras::k_floatx()) * bias
+  )
+  tensorflow::set_random_seed(1337L)
+
   old_weights <- mod$model$get_weights()
+
   tf_initialise_model(mod, global_fit$params, mode = "none")
   expect_identical(mod$model$get_weights(), old_weights)
   tf_initialise_model(mod, global_fit$params, mode = "perturb")
   expect_equal(mod$model$get_weights(), list(old_weights[[1L]], array(bias)), tolerance = 1.0e-7)
   tf_initialise_model(mod, global_fit$params, mode = "scale")
-  expect_equal(mod$model$get_weights(), list(matrix(0.0428100116550922), array(bias)), tolerance = 1.0e-7)
+  expect_equal(mod$model$get_weights(), list(matrix(random_weight), array(bias)), tolerance = 1.0e-7)
   tf_initialise_model(mod, global_fit$params, mode = "zero")
   expect_equal(mod$model$get_weights(), list(matrix(0.0), array(bias)), tolerance = 1.0e-7)
 })
